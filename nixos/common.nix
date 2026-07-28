b59a01d4c44f48de873b92a2e48f92fb742ca073
elias <nobitaelias.06@gmail.com>
2026-07-28 22:35:18 +0200

    Salvataggio automatico

--- nixos/common.nix
+++ nixos/common.nix
@@ -165,7 +165,7 @@ fonts = {
     brightnessctl             # Per regolare la luminosità dello schermo
     hyfetch                  # Per vedere informazioni sul pc
     grim
-    tree-sitter
+    tree
   ];
 
 # Configuriamo greetd per l'autologin fulmineo in Hyprland
