34050feaaf3fe0da958980185e0e5742439b8535
elias <nobitaelias.06@gmail.com>
2026-08-09 14:52:39 +0200

    Salvataggio automatico

--- nixos/common.nix
+++ nixos/common.nix
@@ -169,7 +169,7 @@ nix.settings.experimental-features = [ "nix-command" "flakes" ];
     grim
     tree
     
-    pkgs.wineWoW64
+    pkgs.wineWow64Packages
     # Configurazione di AGS con le librerie Astal necessarie
     (ags.override {
       extraPackages = [
